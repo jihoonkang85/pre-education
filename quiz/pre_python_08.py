850ee6c3d66a525921b4e75a24e69fcac6ce00d1
jihoonkang85 <jihoonkang85@gmail.com>
2020-04-23 02:09:30 +0900

    Answer quiz 08

--- quiz/pre_python_08.py
+++ quiz/pre_python_08.py
@@ -7,3 +7,20 @@
 <출력>
 짝수입니다.
 """
+
+
+def enter():
+    num = input("정수를 입력하세요 : ")
+    try:
+        val = int(num)
+        if (val % 2) == 0:
+            print("짝수입니다.")
+        else:
+            print("홀수입니다.")
+
+    except ValueError:
+        print("정수가 아닙니다.")
+        return enter()
+
+
+enter()
